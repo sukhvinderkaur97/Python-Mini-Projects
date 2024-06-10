2d775ff0fae7178403a4bc7a843b7addb31581bc
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-06-10 17:28:33 +0530

    install libraries

---
+++ Weather App/weather.py
@@ -0,0 +1,8 @@
+from tkinter import *
+import tkinter as tk
+from geopy.geocoders import Nomination
+from tkinter import ttk, messagebox
+from timezonefinder import TimezoneFinder
+from datetime import datetime
+import requests
+import pytz
