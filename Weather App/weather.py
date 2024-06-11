21ee1561f93d8769eee233db7bfeb4363da264a9
Sukhvinder Kaur <sukhvinderkaur2416@gmail.com>
2024-06-11 18:39:12 +0530

    new commit

--- Weather App/weather.py
+++ Weather App/weather.py
@@ -1,8 +1,13 @@
 from tkinter import *
 import tkinter as tk
-from geopy.geocoders import Nomination
+from geopy.geocoders import Nominatim
 from tkinter import ttk, messagebox
 from timezonefinder import TimezoneFinder
 from datetime import datetime
 import requests
 import pytz
+
+root = Tk()
+root.title("Wether App")
+root.geometry("900x500+300+200")
+root.resizable(False,False)
